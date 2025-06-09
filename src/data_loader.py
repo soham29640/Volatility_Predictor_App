bb8da6b7f2095de5c624ff439eb6e76ffccd5e90
soham29640 <soham04x18@gmail.com>
2025-06-09 23:12:52 +0530

    just updating data

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-data = yf.download("AAPL", period="60d", interval="1d")
+data = yf.download("AAPL", period="180d", interval="1d")
 print(data.head())
 
 data.to_csv("data/raw/AAPL.csv")
@@ -10,16 +10,3 @@ data.to_csv("data/raw/AAPL.csv")
 data['log_return'] = np.log(data['Close'] / data['Close'].shift(1))
 data.dropna(inplace=True)
 data.to_csv("data/processed/returns.csv")
-
-data = pd.read_csv("data/processed/returns.csv", index_col=0, parse_dates=True)
-
-log_returns = data['log_return']
-true_volatility = log_returns.rolling(window=5).std()
-
-true_df = pd.DataFrame({
-    "date": true_volatility.index,
-    "true_volatility": true_volatility.values
-})
-
-true_df.dropna(inplace=True)
-true_df.to_csv("outputs/predictions/true_values.csv", index=False)
